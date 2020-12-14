d8875f79fe42683a0e8d1f5acd3d3121034d34d2
lanieehapp <lanieehapp@gmail.com>
2020-12-14 13:55:09 -0800

    multisample merge script

--- multisample_merge.R
+++ multisample_merge.R
@@ -1,4 +1,58 @@
 
 #multisample variant calling merger
 
+args = commandArgs(trailingOnly=TRUE)
+
+filt.file.list<-args[[1]]
+#wl.file.list<-args[[2]]
+
+
+for( i in 1:length(filt.file.list)){
+  curr.samp<-read.table(filt.file.list[1], header=TRUE, sep="\t")
+    
+  #long form - just concatenate all rows, each row represents one sample-variant pair
+  if(!(exists("long.form"))){
+    long.form<-curr.samp
+  }  
+  if(exists("long.form")){
+    long.form<-rbind(long.form, curr.samp)
+  }
+  
+  #wide form - merge variants - each row represents one variants and each sample has their own columns (nCallers, AF, depth)
+  if(!(exists("wide.form"))){
+    
+    wide.form.fixed<-curr.samp[,1:139]
+    samp<-cbind(curr.samp$CHROM_POS_REF_ALT, curr.samp[,grepl("nCallers|afMax|dpMax", colnames(curr.samp))])
+    id<-curr.samp$Sample_ID[1]
+    
+    colnames(samp)[1]<-"CHROM_POS_REF_ALT"
+    colnames(samp)[2:4]<-paste(id, colnames(samp[,2:4]))
+    
+    wide.form.samp<-samp
+  }  
+  if(exists("wide.form")){
+    
+    fixed<-curr.samp[,1:139]
+    samp<-cbind(curr.samp$CHROM_POS_REF_ALT, curr.samp[,grepl("nCallers|afMax|dpMax", colnames(curr.samp))])
+    id<-curr.samp$Sample_ID[1]
+    
+    colnames(samp)[1]<-"CHROM_POS_REF_ALT"
+    colnames(samp)[2:4]<-paste(id, colnames(samp[,2:4]))
+    
+    wide.form.fixed<-unique(rbind(wide.form.fixed, fixed))
+   
+    wide.form.samp<-merge(x=wide.form.samp, y=samp, by="CHROM_POS_REF_ALT", all.x=TRUE, all.y=TRUE)
+
+  }
+  
+}
+
+wide.form.fixed<-wide.form.fixed[order(wide.form.fixed$CHROM_POS_REF_ALT),]
+wide.form.samp<-wide.form.samp[order(wide.form.samp$CHROM_POS_REF_ALT),]
+
+wide.form<-merge(x=wide.form.fixed, y=wide.form.samp, by="CHROM_POS_REF_ALT")
+
+write.table(long.form, file=args[2], row.names = FALSE, quote = FALSE, sep="\t")
+write.table(wide.form, file=args[3], row.names = FALSE, quote = FALSE, sep="\t")
+
 
